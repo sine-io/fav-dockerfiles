b9e59f83e23dc74fac50afcfaa48b12deb25ba0d
sine <sinecelia.wang@gmail.com>
2022-07-15 10:27:54 +0800

    modify alpine, and change cosbench tar

--- cosbench-ehualu/examples/run-driver-example.sh
+++
@@ -1,8 +0,0 @@
-
-
-docker run -dit --restart unless-stopped --net=host \
--e CONTROLLER=false \
--e DRIVER=true \
--e DRIVER_PORT=18088 \
--e COSBENCH_PLUGINS="ESS" \
-sinecelia/cosbench-ehualu:0.4.7.8-ubuntu
