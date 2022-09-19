c44097a08b27b2a0cfee6e661cb8630638e3a154
sine <sinecelia.wang@gmail.com>
2022-09-19 17:34:33 +0800

    add build docker images script.

---
+++ cosbench-sineio/docker/build-and-push.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+
+source ./.env
+
+
+if [ ! -f cosbench-$VERSION-sineio.tar.gz  ]
+then
+	echo "Get cosbench-${VERSION}-sineio.tar.gz from github"
+	wget https://github.com/sine-io/cosbench-sineio/releases/download/$VERSION/cosbench-$VERSION-sineio.tar.gz
+	echo "Done"
+fi
+
+
+echo "Build docker image of ubuntu..."
+docker build -t sinecelia/cosbench-sineio:$VERSION-ubuntu -f Dockerfile-ubuntu . --build-arg COS_VERSION=$VERSION
+echo "Done"
+
+echo "Build docker image of alpine..."
+docker build -t sinecelia/cosbench-sineio:$VERSION-alpine -f Dockerfile-alpine . --build-arg COS_VERSION=$VERSION
+echo "Done"
+
+echo "Push to the Docker Hub"
+docker push sinecelia/cosbench-sineio:$VERSION-ubuntu
+docker push sinecelia/cosbench-sineio:$VERSION-alpine
+
+echo "Done"
+
+rm -f ./cosbench-$VERSION-sineio.tar.gz
